4920ac2fcaf8e07db26aaca62a29621b29f04ac4
Lucas Moura <lucas3ms.ls@gmail.com>
2023-01-05 09:00:45 -0300

    feat: add validator tag

--- domain/model/user.go
+++ domain/model/user.go
@@ -7,30 +7,32 @@ import (
 )
 
 type User struct {
-    Name        string      `json:"name" bson:"name"`
-    Email       string      `json:"email" bson:"email"`
-    Password    string      `json:"password" bson:"password"`
+    Name        string      `json:"name"`
+    Email       string      `json:"email"`
+    Password    string      `json:"password"`
 }
 
 type UserModel struct {
     Name        string      `json:"name" bson:"name"`
     Email       string      `json:"email" bson:"email"`
     Password    string      `json:"password" bson:"password"`
-    CreatedAt   time.Time   `json:"created_at" bson:"created_at"`
-    UpdatedAt   time.Time   `json:"updated_at" bson:"updated_at"`
+    CreatedAt   time.Time   `json:"created_at" bson:"created_at,omitempty"`
+    UpdatedAt   time.Time   `json:"updated_at" bson:"updated_at,omitempty"`
 }
 
 type DBUserResponse struct {
     Id          primitive.ObjectID  `json:"id" bson:"_id"`
-    Name        string      `json:"name" bson:"name"`
-    Email       string      `json:"email" bson:"email"`
-    Password    string      `json:"password" bson:"password"`
-    CreatedAt   time.Time   `json:"created_at" bson:"created_at"`
-    UpdatedAt   time.Time   `json:"updated_at" bson:"updated_at"`
+    Name        string              `json:"name" bson:"name"`
+    Email       string              `json:"email" bson:"email"`
+    Password    string              `json:"password" bson:"password"`
+    CreatedAt   time.Time           `json:"created_at" bson:"created_at"`
+    UpdatedAt   time.Time           `json:"updated_at" bson:"updated_at"`
 }
 
 type UserInput struct {
-    User
-    PasswordConfirm    string    `json:"password"`
+    Name                string      `json:"name" validate:"required,min=4,max=15"`
+    Email               string      `json:"email" validate:"required,email"`
+    Password            string      `json:"password" validate:"required,min=6,containsany=!@#?*"`
+    PasswordConfirm     string      `json:"passwordConfirm" validate:"eqfield=Password"`
 }
 
